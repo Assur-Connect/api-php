810f62d3f2683b762f858c9dc601d16603b44ec7
agnonym <contact@agnonym.com>
2021-09-20 12:48:37 +0200

    * | Exception - Api: add getBody() / getResponse()

--- src/Exceptions/ApiException.php
+++ src/Exceptions/ApiException.php
@@ -4,12 +4,17 @@ namespace AssurConnect\Api\Exceptions;
 
 class ApiException extends \Exception
 {
+    private $body = null;
+    private $response = null;
+
     public function __construct(
         string $messageSource = '',
         int $code = 0,
         $requestBody = null,
         $response = null
     ) {
+        $this->code = $code;
+
         $message = '[' . (new \DateTimeImmutable())->format(\DateTime::ISO8601) . '] ' . 'Error Code: ' . $code;
 
         if ($messageSource !== '') {
@@ -17,13 +22,25 @@ class ApiException extends \Exception
         }
 
         if ($requestBody) {
+            $this->body = $requestBody;
             $message .= PHP_EOL . 'Request body: ' . print_r($requestBody, true);
         }
 
         if (!empty($response)) {
+            $this->response = $response;
             $message .= PHP_EOL . 'Response body: ' . print_r($response, true);
         }
 
         parent::__construct($message, $code);
+    }
+
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    public function getResponse()
+    {
+        return $this->response;
     }
 }
